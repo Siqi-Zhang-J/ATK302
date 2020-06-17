788adda88d590627c17fcb22899ef756430f1a35
SiqiZhang <kc_zqq@126.com>
2020-06-18 01:11:36 +0800

    first time

--- p5/Unit1/scene/sketch.js
+++ p5/Unit1/scene/sketch.js
@@ -1,21 +1,34 @@
 function setup() {
-  createCanvas(500, 500);
-  
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(100) ; 
-  // put your shapes here!
-  
-  
-  text(mouseX + " , " + mouseY, 10, 20) ;  // this helps know where the mouse is
-} 
 
-  
+  background("black");
+  noStroke();
 
-function mouseReleased() { 
-  // if you click the mouse, you'll see the location in the debugging console
-  print(mouseX + " , " + mouseY) ; 
-} 
+ fill(250)
+ triangle(100, 18, 18, 380, 81, 380);
 
- 
+ fill('250')
+ ellipse(200,100,100,100)
+ ellipse(160,130,90,90)
+ ellipse(320,130,90,90)
+fill('250');
+  triangle(18, 18, 18, 380, 81, 380);
+ellipse(250,100,100,100)
+  ellipse(300,110,100,100)
+fill('blue');
+  rect(300, 230, 73, 73)
+fill('blue');
+  rect(250, 270, 73, 73)
+fill('250');
+  ellipse(260, 144, 80, 80)
+fill('250');
+  ellipse(222, 144, 72, 72)
+fill('blue')
+  rect(200, 330, 100, 50)
+
+fill('250');
+  quad(130, 250, 180, 250, 270, 380, 130, 380)
+ ellipse(328,328,110,110)}
