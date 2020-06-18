914161c85eb99cc23320cf838fc9508f25557474
SiqiZhang <kc_zqq@126.com>
2020-06-19 03:27:18 +0800

    Go

--- p5/Unit1/empty-example/sketch.js
+++ p5/Unit1/empty-example/sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-
+  createCanvas(400, 400);
 }
 
 function draw() {
 
-}
+ background("black");
+  noStroke();
+
+fill('250')
+ ellipse(200,250,200,200)
+ellipse(140,140,80,200)
+ellipse(245,140,80,200)
+fill('pink')
+ellipse(140,130,40,100)
+ellipse(245,130,40,100)
+  fill('grey')
+ rect(260, 300, 100, 5)
+ rect(260, 250, 100, 5)
+ rect(260, 275, 100, 5)
+ rect(40, 250, 100, 5)
+rect(40, 300, 100, 5)
+rect(40, 275, 100, 5)
+fill('black')
+ ellipse(200,280,20,20)
+  fill('brown')
+ ellipse(230,240,12,12)
+ ellipse(170,240,12,12)
+  fill('orange')
+ triangle(55, 180, 80, 80, 25, 80)
+  fill('green')
+triangle(40, 100, 68, 70, 100, 60)
+triangle(40, 100, 38, 60, 50, 60)
+triangle(40,40, 58, 60, 60, 100)}
