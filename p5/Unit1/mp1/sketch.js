fe096979fa0220ab37037711a98f52a40bc3f282
SiqiZhang <kc_zqq@126.com>
2020-06-21 21:11:39 +0800

    mp1

--- p5/Unit1/mp1/sketch.js
+++ p5/Unit1/mp1/sketch.js
@@ -1,91 +1,62 @@
-function setup() { 
-
-
-
-  createCanvas(1000, 800);
-
-  rectMode(CENTER);
-
-  ellipseMode(CENTER);
-
-
-
-
-
+function setup() {
+  createCanvas(400, 400);
 }
-
-
-
-
 
 function draw() {
 
+ background("black");
+  noStroke();
 
-
-  background(100);
-
-
-
-  // stuff that we always want to show in back
-
-
-
-
-
-
-
-  if (mouseIsPressed) {
+fill('250')
+ ellipse(200,250,200,200)
+ellipse(140,140,80,200)
+ellipse(245,140,80,200)
+fill('pink')
+ellipse(140,130,40,100)
+ellipse(245,130,40,100)
+  fill('grey')
+ rect(260, 300, 100, 5)
+ rect(260, 250, 100, 5)
+ rect(260, 275, 100, 5)
+ rect(40, 250, 100, 5)
+rect(40, 300, 100, 5)
+rect(40, 275, 100, 5)
+fill('black')
+ ellipse(200,280,20,20)
+  fill('brown')
+ ellipse(230,240,12,12)
+ ellipse(170,240,12,12)
+  fill('orange')
+ triangle(55, 180, 80, 80, 25, 80)
+  fill('green')
+triangle(40, 100, 68, 70, 100, 60)
+triangle(40, 100, 38, 60, 50, 60)
+triangle(40,40, 58, 60, 60, 100)
+if (mouseIsPressed) {
 
     // stuff that shows when the mouse is pressed
-
-    ellipse(100, 100, 100, 100);
-
-    text("PRESSED MOUSE!!!", 200, 300);
-
+fill('orange')
+ triangle(55, 180, 80, 80, 25, 80)
+  fill('green')
+triangle(40, 100, 68, 70, 100, 60)
+triangle(40, 100, 38, 60, 50, 60)
+triangle(40,40, 58, 60, 60, 100);
+fill('orange')
+    text("CARROT", 260, 350);
 
 
   } else {
 
     // when the mouse isn't pressed!
 
-    rect(100, 100, 100, 100);
+  fill('green')
+ triangle(55, 180, 80, 80, 25, 80)
+  fill('orange')
+triangle(40, 100, 68, 70, 100, 60)
+triangle(40, 100, 38, 60, 50, 60)
+triangle(40,40, 58, 60, 60, 100) ;
+fill('white')
+    textSize(30);
 
-    textSize(40);
-
-    text("The mouse isn't pressed.", 200, 300);
-
-  }
-
-
-
-
-
-
-
-  // stuff that will always show in front
-
-
-
-
-
-  // this shows mouse location - comment it out when you're done!
-
-  fill(0);
-
-  text(mouseX + ", " + mouseY, 40, 40);
-
-
-
-}
-
-
-
-
-
-
-
-function mouseReleased() {
-
-  print(mouseX + ", " + mouseY);
-
-}
+    text("RABBIT", 260, 350);
+}}
