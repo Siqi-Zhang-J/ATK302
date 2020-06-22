e071484a22b1794b961ed2ac659c129b978bbbe8
SiqiZhang <kc_zqq@126.com>
2020-06-23 01:17:55 +0800

    mp2

--- p5/Unit2/empty-example/sketch.js
+++ p5/Unit2/empty-example/sketch.js
@@ -1,7 +1,47 @@
+let state = 0
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(300,300);
 }
 
 function draw() {
 
+  switch(state){
+    case 0:
+     background('yellow');
+      fill('orange')
+      ellipse(56, 50, 55, 55);
+           ellipse(75, 50, 50, 50);
+           ellipse(90, 50, 45, 45);
+      ellipse(105, 50, 40, 40);
+            ellipse(160, 250, 55, 55);
+           ellipse(170, 250, 50, 50);
+           ellipse(185, 250, 45, 45);
+      ellipse(200, 250, 40, 40);
+
+      fill('black')
+     text('Enjoy the summer!!!', 100, 150);
+      fill('white')
+      triangle(30, 175, 58, 135, 86, 175);
+      square(220, 120, 55);
+     break;
+
+    case 1:
+     background('orange');
+     text('But summer is so hot', 90, 150);
+     break;
+
+    case 2:
+     background('red');
+     text('And there will be a lot of rain', 75, 150);
+     break;
+  }
+
+}
+
+function mouseReleased(){
+  state = state +1;
+  if(state > 2){
+    state = 0;
+  }
 }
