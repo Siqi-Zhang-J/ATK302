998899e08bd2ef145b8f1a5a27ec9546ddbc0a3f
SiqiZhang <kc_zqq@126.com>
2020-07-02 02:46:52 +0800

    oo

--- p5/Unit4/gyro/sketch.js
+++ p5/Unit4/gyro/sketch.js
@@ -8,6 +8,8 @@ var yPosition = 0;
 var x = 0; // acceleration data
 var y = 0;
 var z = 0;
+let img1 ;
+let img2 ;
 
 // other variables
 var bunnyImage;
@@ -15,11 +17,20 @@ var bunnyImage;
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
+  img1 = loadImage("assets/1.jpg") ;
+  img2 = loadImage("assets/2.png") ;
 
   // initialize accelerometer variables
   alpha = 0;
   beta = 0;
   gamma = 0;
+
+for (var 1 = 0;1 < 30;1++){
+  cars.push(new Car());
+}
+
+frogPos = createVector(width / 2,height -80);
+
 
 // initialize other variables
   bunnyImage = loadImage("assets/1ZY.jpg");
@@ -31,7 +42,7 @@ function setup() {
 function draw() {
 
   background('#c6f5fe'); // light blue
-    // add an image for the background?
+  image(img1,width/2,height/2,width,height) ;
 
   // the map command !!!!
   // takes your variable and maps it from range 1 to range 2
@@ -44,18 +55,29 @@ function draw() {
   push(); // before you use translate, rotate, or scale commands, push and then pop after
   translate(xPosition, yPosition); // move everything over by x, y
   // rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
-  image(bunnyImage, 0, 0, 500, 500);
   //  	rect(0, 0, 100, 100) ;
-  pop();
 
 
   // Here is where you can design this program!
+  image(img2,width/2,height/2,width,height) ;
+  pop();
+
+
+  frogPos.x = xPosition;
+  frogPos.y = yPosition;
+
+  for (var i = 0; i < array.length; i++) {
+   cars[i].display();
+   cars[i].drive();
+   if (cars[i].pos.dist(frogPos)<50){
+     cars.splice,(1,1);
+   }
+  }
+
   fill('white');
-  noStroke();
   textSize(80);
   textAlign(CENTER);
-  text("Squidward", width / 2, height / 2);
-
+  text("Squidward"，width/2，600，windowWidth - 200,windowHeight - 200);
   // DECORATIONS
   // Just a bunch of text commands to display data coming in from addEventListeners
   textAlign(LEFT);
@@ -77,6 +99,13 @@ function draw() {
 
 }
 
+function deviceShaken(){
+  cars = [];
+  for (var i = 0;i < 40;i++){
+    cars.push(new Car())
+  }
+}
+
 // HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
 
 // Read in accelerometer data
@@ -94,3 +123,37 @@ window.addEventListener('devicemotion', function(e) {
   y = e.acceleration.y;
   z = e.acceleration.z;
 });
+
+
+
+
+
+
+function car(){
+  this.pos = createVector(100,100);
+  this.vel = createVector(random(-5,5),random(-5,5));
+  this.r = random(255);
+    this.g = random(255);
+      this.b = random(255);
+        this.a = random(255);
+
+
+        this.display = function(){
+          fill(this.r,this.g,this.a);
+          ellipse(this.pos.x,this.pos.y,50,50);
+          ellipse(this.pos.x + 35,this.pos.y,50,50);
+          rect(this.pos.x + 20,this.pos.y - 30,80,60);
+
+        }
+
+        this.drive = function(){
+          this.pos.add(this.vel);
+
+          if (this.pos.x > width) this.pos.x = 0;
+          if (this.pos.x < 0) this.pos.x = width;
+          if (this.pos.y > height) this.pos.y = 0;
+          if (this.pos.y < 0) this.pos.y = height;
+
+
+        }
+}
