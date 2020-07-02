5a9aa291109ae9e5172862910524baeff46ec353
SiqiZhang <kc_zqq@126.com>
2020-07-03 01:29:25 +0800

    123

--- p5/Unit3/mp3/sketch.js
+++ p5/Unit3/mp3/sketch.js
@@ -1,11 +1,16 @@
 var cars = [];
 var picture = []; // an array for the objects
-var frogPos ;
-let state = -1 ;
-let timer = 0 ;
-let img1 ;
-let img2 ;
+var frogPos;
+let state = -1;
+let timer = 0;
+let img1;
+let img2;
+let bubbles;
+let gamebg;
 let song1, song2, song3;
+let maxBirds = 10;
+
+let font;
 
 function preload() {
   song1 = loadSound("assets/1.mp3");
@@ -18,22 +23,27 @@ function preload() {
   song2.pause();
   song3.pause();
 
-
+// font = loadFont("assets/.......") ;
 }
 
 function setup() {
 
-  createCanvas(446, 311) ;;
-  img1 = loadImage("assets/2.jpeg") ;
-  img2 = loadImage("assets/1.jpg") ;
+//  createCanvas(446, 311);;
+  createCanvas(892, 622);;
+//  textFont(font, 16);
+
+  img1 = loadImage("assets/2.jpeg");
+  img2 = loadImage("assets/1.jpg");
+  bubbles = loadImage("assets/bubbles.png");
+  gamebg = loadImage("assets/gamebg.png");
   //
 
-picture[0] = loadImage("assets/hmbb.png");
-picture[1] = loadImage("assets/zyg.png");
+  picture[0] = loadImage("assets/hmbb.png");
+  picture[1] = loadImage("assets/zyg.png");
 
   // spawn the objects
-  for (let i = 0; i < 10; i++) {
-    cars.push(new Car());  // put the objects onto the cars array
+  for (let i = 0; i < maxBirds; i++) {
+    cars.push(new Car()); // put the objects onto the cars array
   }
   frogPos = createVector(width / 2, height - 120);
   rectMode(CENTER);
@@ -47,80 +57,101 @@ picture[1] = loadImage("assets/zyg.png");
 
 function draw() {
 
-  switch(state) {
+  switch (state) {
 
     case -1:
-    song1.play() ;
-    state = 0 ;
-    break ;
+      song1.play();
+      state = 0;
+      break;
 
     case 0:
-    background('grey') ;
-   image(img2,width/2,height/2,width,height) ;
-   fill('black');
-    text("welcome to my game!", 150, 200) ;
-    break ;
+      background('grey');
+      image(img2, width / 2, height / 2, width, height);
+      fill('black');
+      text("welcome to my game!", 150, 200);
+      break;
 
-    case 1:  //game state
-      game() ;
-      timer++ ;
-      if (timer > 200) {
-        state = 3 ;
-        timer = 0 ;
+    case 1: //game state
+      game();
+      timer++;
+      if (timer > 500) {
+        state = 3;
+        stopTheSongs();
+        song3.play();
+        timer = 0;
       }
-      break ;
+
+      break;
 
     case 2:
-    background('pink') ;
-    image(img2,width/2,height/2,width,height) ;// put image here
-    fill('black') ;
-    text("You won!!!", 150, 200) ;
-    break ;
+      background('pink');
+      image(img2, width / 2, height / 2, width, height); // put image here
+      fill('black');
+      text("You won!!!", 150, 200);
+      break;
 
     case 3:
-      background('blue') ;
-      image(img1,width/2,height/2,width,height) ;// put image here
-      fill('black') ;
-      text("Sorry,you are lost,try again!", 150, 200) ;
-      break ;
+      background('blue');
+      image(img1, width / 2, height / 2, width, height); // put image here
+      fill('black');
+      text("Sorry,you are lost,try again!", 150, 200);
+      break;
 
   }
 }
 
 function reset() {
-  cars = [] ;
-  timer = 0 ;
+  cars = [];
+  timer = 0;
   // spawn the objects
-  for (let i = 0; i < 10; i++) {
-    cars.push(new Car());  // put the objects onto the cars array
+  for (let i = 0; i < maxBirds; i++) {
+    cars.push(new Car()); // put the objects onto the cars array
   }
 }
 
+function stopTheSongs() {
+  song1.pause();
+  song2.pause();
+  song3.pause();
+}
+
+function resetTheGame() {
+  cars = [];
+  // spawn cars!!!
+  for (var i = 0; i < maxBirds; i++) {
+    cars.push(new Car());
+  }
+  timer = 0;
+
+}
+
+
 function mouseReleased() {
-    switch(state) {
-      case 0:
-      state = 1 ;
-      break ;
+  switch (state) {
+    case 0: // click to go to the game state
+      stopTheSongs();
+      song2.play();
+      state = 1;
+      break;
 
-      case 2: // win
-      reset() ;
-      state = 0 ; // go back to welcome
-      break ;
+    case 2: // the win state!
+      stopTheSongs();
+      resetTheGame();
+      state = -1;
+      break;
 
-      case 3:  // lose
-      reset() ;
-      state = 0 ; //go back to welcome
-      break ;
-
-
-    }
+    case 3: // the lose myState
+      stopTheSongs();
+      resetTheGame();
+      state = -1;
+      break;
 
   }
-
+}
 
 function game() {
   background('white');
-  // image ...
+  image(gamebg, width / 2, height / 2, width, height);
 
   // iterate through the array, display them, update them
   for (let i = 0; i < cars.length; i++) {
@@ -128,25 +159,25 @@ function game() {
     cars[i].update();
 
     if (cars[i].pos.dist(frogPos) < 50) {
-      cars.splice(i, 1) ; //delate a car
+      cars.splice(i, 1); //delate a car
     }
   }
 
   // check if therenare any cars left
   if (cars.length == 0) {
-    state =  2; //go to win state}
+    state = 2; //go to win state}
   }
 
 
   // draw a "frog" here
-  fill('green') ;
-//  ellipse(frogPos.x, frogPos.y, 70, 70) ;
+  fill('green');
+  //  ellipse(frogPos.x, frogPos.y, 70, 70) ;
   image(picture[0], frogPos.x, frogPos.y, 70, 70);
 
   //    checkForKeys() ;
 
-  frogPos.x = mouseX ;
-  frogPos.y = mouseY ;
+  frogPos.x = mouseX;
+  frogPos.y = mouseY;
 
 }
 
@@ -164,7 +195,7 @@ function checkForKeys() {
 class Car {
   constructor() {
     // attributes
-    this.pos = createVector(width / 2, height / 2); // where it starts
+    this.pos = createVector(random(width), random(height)); // where it starts
     this.vel = createVector(random(-3, 3), random(-3, 3)); // direction
     this.r = random(255);
     this.g = random(255);
@@ -176,13 +207,13 @@ class Car {
   // methods
 
   display() { // this displays the object
-    fill(this.r, this.g, this.b, this.a);
-    ellipse(this.pos.x, this.pos.y, 30, 30);
-    // image(img1, this.pos.x, this.pos.y, 100, 100) ;
+    // fill(this.r, this.g, this.b, this.a);
+    // ellipse(this.pos.x, this.pos.y, 30, 30);
+    image(bubbles, this.pos.x, this.pos.y, 100, 100);
   }
 
   update() { // this moves the object
-    this.pos.add(this.vel);  // add the velocity to the position
+    this.pos.add(this.vel); // add the velocity to the position
 
     // loop the objects around the screen
     if (this.pos.x > width) this.pos.x = 0;
@@ -191,4 +222,8 @@ class Car {
     if (this.pos.y < 0) this.pos.y = height;
   }
 
+}
+
+function touchStarted() {
+  getAudioContext().resume();
 }
